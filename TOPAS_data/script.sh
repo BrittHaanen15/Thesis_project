4c84548ca69057b694d4e5af981eb8cf60f40272
BrittHaanen15 <bshaanen@uio.no>
2026-03-24 14:46:21 +0100

    Set up the architecture for processing TOPAS/MONAS data

---
+++ TOPAS_data/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mv MKModel_SMKM.csv ./OUTPUTS_MONASTEST/MKModel_SMKM.csv
+mv MKModel_StaurationCorrected.csv ./OUTPUTS_MONASTEST/MKModel_StaurationCorrected.csv
+
+rm y*
+
+mv OUTPUTS_MONASTEST /mnt/c/Users/linux/Documents/Thesis_project/TOPAS_data/
